69d40eafbc20d486164c1e66f2bb6037a4d1d8d1
HubArtWork <68958381+HubArtWork@users.noreply.github.com>
2022-11-04 16:14:07 +0200

    massager vmnvxncoivwwc

---
+++ massager/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  //scroll link
+  $('.scroll-link').on('click', function(e){
+    e.preventDefault();
+    var itemId = $(this).attr('href'),
+        blockTop = $(itemId).offset().top;
+    $('html, body').animate({scrollTop : blockTop},900);
+  });
+
+  var swiper = new Swiper('.testimonials-slider', {
+    slidesPerView: 1,
+    spaceBetween: 14,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+});
